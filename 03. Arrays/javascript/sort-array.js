01fa2b23b5b9ea5791f8283c898b77b15b80d7eb
Yatheswar546 <yatheswar546@gmail.com>
2026-05-13 23:48:05 +0530

    sort-array-js

---
+++ 03. Arrays/javascript/sort-array.js
@@ -0,0 +1,38 @@
+// Problem Description
+// Given an array of n strings. Sort the array in lexicographical order.
+
+// Input format
+// First line contains integer n.
+
+// Next line contains n space separated strings.
+
+// Output format
+// Print the sorted array.
+
+// Sample Input 1
+// 4
+
+// abc abd ab a
+
+// Sample Output 1
+// a ab abc abd
+
+// Explanation
+// The given array is sorted in lexicographical order.
+
+// Constraints
+// 1 <= n <= 10000
+
+// Strings contain only lowercase english alphabet.
+
+// Each string will have length no more than 15.
+
+// Code 
+
+function sortArray(arr) {
+
+    arr.sort()
+
+    return arr;
+
+}
