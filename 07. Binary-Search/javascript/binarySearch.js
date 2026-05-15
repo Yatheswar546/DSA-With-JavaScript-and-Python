5503e74bc7adea99087a812887dd48ffbf81eb98
Yatheswar546 <yatheswar546@gmail.com>
2026-05-16 01:35:40 +0530

    binary-search-js

---
+++ 07. Binary-Search/javascript/binarySearch.js
@@ -0,0 +1,34 @@
+// Problem Description 
+
+// Implement Binary Search
+
+function binarySearch(arr, target) {
+
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+
+        if(arr[mid] ===  target) {
+            return true
+        }
+        else if(arr[mid] > target) {
+            right = mid + 1;
+        }
+        else if(arr[mid] < target) {
+            left = mid - 1;
+        }
+    }
+    return false;
+
+}
+
+binarySearch([1,2,3,4,5], 1);
+
+/*
+    Time Complexity  : O(log n)
+    Space Complexity : O(1)
+*/
+ 
