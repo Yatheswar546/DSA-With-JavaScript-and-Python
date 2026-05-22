2eb9285cc5f7ad5e98faf5fbf262764ddce0f94d
Yatheswar546 <yatheswar546@gmail.com>
2026-05-23 03:24:30 +0530

    fibonacci-number-js

---
+++ 08. Recursion/javascript/fibonacciNumber.js
@@ -0,0 +1,66 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+// Example 2:
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+// Example 3:
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+// Constraints:
+// 0 <= n <= 30
+
+
+// Recursive Approach
+function fibonacciNumber(num) {
+
+    if(num <= 1) {
+        return num;
+    }
+
+    else {
+        return fibonacciNumber(num-1) + fibonacciNumber(num-2);
+    }
+}
+
+/*
+    Time Complexity  : O(2^n)
+    Space Complexity : O(n)
+*/
+
+
+// Iterative Approach
+function fibonacciNumber(n) { 
+
+    if(n <= 1) {
+        return n;
+    }
+
+    let a = 0, b = 1, c = 0;
+
+    for(let i=2; i<=n; i++) {
+
+        c = a+b;
+        a = b;
+        b = c;
+    }
+
+    return c;
+}
+
+/*
+    Time Complexity  : O(n)
+    Space Complexity : O(1)
+*/
