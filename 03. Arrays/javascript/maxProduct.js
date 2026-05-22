28ef1a66a2f6d3c32d15ecfb83899f502372d65e
Yatheswar546 <yatheswar546@gmail.com>
2026-05-23 02:49:09 +0530

    max-product-js

---
+++ 03. Arrays/javascript/maxProduct.js
@@ -0,0 +1,76 @@
+// Problem Description
+// Given the array of integers nums of size n, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+// Input format
+// First line contains n, the number of distinct integers.
+// Second line contains n space separated integers
+
+// Output format
+// Print the maximum product.
+
+// Sample Input 1
+// 4
+// 3 4 5 2
+
+// Sample Output 1
+// 12
+
+// Explanation
+// If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)(nums[2]-1) = (4-1)(5-1) = 3*4 = 12.
+
+// Sample Input 2
+// 4
+// 1 5 4 5
+
+// Sample Output 2
+// 16
+
+// Explanation
+// Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+
+// Constraints
+// 2 <= n <= 500
+// 1 <= nums[i] <= 10^3
+
+// Code
+
+function maxProduct(nums) {
+
+    let n = nums.length
+
+    let max1 = 0
+    let max2 = 0
+    let count = 1
+
+    for(let i=0; i<n; i++) {
+
+        if (nums[i] == max1) {
+            count++;
+        }
+
+        else if(nums[i] > max1) {
+            max1 = nums[i]
+            count = 1;
+        }
+    }
+
+    if(count >= 2) {
+        return (max1-1) * (max1-1)
+    }
+
+    else {
+        for(let i=0; i<n; i++) {
+
+            if(nums[i] > max2 && nums[i] < max1) {
+                max2 = nums[i]
+            }
+        }
+
+        return (max1-1) * (max2-1)
+    }
+}
+
+/*
+    Time Complexity  : O(n)
+    Space Complexity : O(1) 
+*/
